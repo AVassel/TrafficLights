2699816bedd44ebf6dae01ca1350787f82458bcd
Anton Vassel <ant-vassel@ya.ru>
2022-03-30 15:21:15 +0300

    Working logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet var lights: [UIView]!
+    
+    var lightIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 15
+        startButton.setTitle("START", for: .normal)
+
+        for light in lights {
+            light.layer.cornerRadius = 35
+            light.alpha = 0.3
+        }
     }
 
-
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        if lights[lightIndex].alpha == 1 {
+            lights[lightIndex].alpha = 0.3
+            lightIndex += 1
+        }
+        
+        if lightIndex > 2 {
+            lightIndex = 0
+        }
+        lights[lightIndex].alpha = 1
+    }
+    
 }
 
+    
